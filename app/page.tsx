7de2b8ecbac2b2cafb5e9f6e2012809897a4382c
younesbelarbi <ybelarbi@outlook.com>
2024-12-11 20:36:24 +0100

    feat(Page): setting up film display

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useMovieList from "@/hooks/useMovieList";
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
