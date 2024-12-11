b95fad01771fd3bfcdbf7d2b1971bb19a2118840
younesbelarbi <ybelarbi@outlook.com>
2024-12-11 20:20:51 +0100

    feat(Prisma): process to get random movie + movie List

---
+++ pages/api/randomMovie.ts
@@ -0,0 +1,32 @@
+import prisma from "@/lib/prismadb";
+import serverAuth from "@/lib/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    await serverAuth(req);
+
+    const movieCount = await prisma.movie.count();
+    const randomIndex = Math.floor(Math.random() * movieCount);
+    const randomMovies = await prisma.movie.findMany({
+      take: 1, // Limit to 1 movie
+      skip: randomIndex, // Skip to the random index
+    });
+
+    if (randomMovies.length === 0) {
+      return res.status(404).json({ message: "No movies found" });
+    }
+
+    return res.status(200).json(randomMovies[0]);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
---
+++ src/hooks/useBillboard.ts
@@ -0,0 +1,13 @@
+import fetcher from "@/lib/fetcher";
+import useSWR from "swr";
+
+const useBillboard = () => {
+  const { data, error, isLoading } = useSWR("/api/randomMovie", fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+  return { data, error, isLoading };
+};
+
+export default useBillboard;
